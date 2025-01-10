dfbefa94e94b06d538ab8893b13f1f123bff03dc
shrouk <shroukdawood1990@gmail.com>
2025-01-10 21:36:53 +0200

    commit

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import { MainApi } from '../../Api/Api';
 import { useProductContext } from '../../contexts/ProductContext';
 
 export default function Products() {
  console.log("Mount1") ;
 //   const [products , setProducts] = useState(null);
   const [testState , setTestState] = useState (true);
-const {products , getProducts}= useProductContext();
+const {products , getProducts ,getSingleproduct}= useProductContext();
+const [ singleProduct , setSingleProduct ] = useState(null);
  useEffect(() => {
  getProducts() } , [testState ]);
  console.log("Mount2") ;
-    return (
+   
+const handelShowSingleproduct = () => {
+    getSingleproduct(5, (data) => {
+        setSingleProduct(data)
+    });
+}
+ return (
     <div>
      <button onClick={() => setTestState((prev) => !prev)}> Change State</button>
-
+<button onClick={handelShowSingleproduct}>SingleProduct</button>
+{singleProduct && <div>
+    <h2>{setSingleProduct.title}</h2>
+    
+    </div>}
         {console.log("Render")}
         {!products && <div>loading......</div>}
         {products && products.map((product,i) => <div key={i}>{product.title}</div>)}
--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -4,6 +4,7 @@ import { MainApi } from "../Api/Api";
 const ProductContext = createContext ({
     products:null,
     getProducts:() => {},
+    getSingleproduct : () => {},
 });
 export const ProductProvider = ({children}) => {
    
@@ -14,8 +15,12 @@ export const ProductProvider = ({children}) => {
     MainApi.get(`/products?limit=${limit}`)
        .then(({data}) => setProducts(data.products));
 };
-   
-   return <ProductContext.Provider value={{products , getProducts}}>
+   const getSingleproduct = (id=1 , cb) => {
+    MainApi.get(`/products/${id}`).then(({data}) => {
+        cb(data)
+    });
+   };
+   return <ProductContext.Provider value={{products , getProducts , getSingleproduct}}>
         {children}
     </ProductContext.Provider>
 };
